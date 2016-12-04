2988bcbd57a5b678c4c2556a5c6698a6f87fad3c
DESKTOP-598A969\phani <Phanikumar_rentachintala@Live.com>
2016-12-04 19:29:09 +1100

    Marketing Gear V0

---
+++ MarketGear/MarketGear.CMS.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarketGear.CMS.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
